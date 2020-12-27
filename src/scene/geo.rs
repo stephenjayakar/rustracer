baf7892738f056a2af0881b76bd8f832facef736
Stephen Jayakar <aj12ay@gmail.com>
2020-12-26 16:27:47 -0800

    Replacing my math internals with `nalgebra` (#6)
    Wanted to have support for performant matrix multiplication and didn't want to implement it. Kept my interface the same mostly so that I could swap out if I needed to.

--- src/scene/geo.rs
+++ src/scene/geo.rs
@@ -1,3 +1,8 @@
+extern crate nalgebra as na;
+
+use na::base::Vector3;
+use na::geometry::Point3;
+
 use std::ops::{Add, Sub};
 
 #[derive(Clone, Copy, Debug)]
@@ -23,62 +28,77 @@ impl Ray {
 
 #[derive(Clone, Copy, Debug)]
 pub struct Point {
-    pub x: f64,
-    pub y: f64,
-    pub z: f64,
+    p: Point3<f64>,
 }
 
 impl Point {
     pub fn new(x: f64, y: f64, z: f64) -> Point {
-        Point { x, y, z }
+        let p = Point3::new(x, y, z);
+        Point::new_from_na(p)
+    }
+
+    fn new_from_na(p: Point3<f64>) -> Point {
+        Point { p }
+    }
+
+    pub fn x(&self) -> f64 {
+        self.p[0]
+    }
+
+    pub fn y(&self) -> f64 {
+        self.p[1]
+    }
+
+    pub fn z(&self) -> f64 {
+        self.p[2]
     }
 }
 
 #[derive(Clone, Copy, Debug)]
 pub struct Vector {
-    pub x: f64,
-    pub y: f64,
-    pub z: f64,
-}
-
-fn norm(x: f64, y: f64, z: f64) -> f64 {
-    f64::sqrt(f64::powi(x, 2) + f64::powi(y, 2) + f64::powi(z, 2))
+    v: Vector3<f64>,
 }
 
 impl Vector {
     pub fn new(x: f64, y: f64, z: f64) -> Vector {
-        Vector { x, y, z }
+        let v = Vector3::new(x, y, z);
+        Vector::new_from_na(v)
+    }
+
+    fn new_from_na(v: Vector3<f64>) -> Vector {
+        Vector { v }
     }
 
     pub fn new_normalized(x: f64, y: f64, z: f64) -> Vector {
-        let mut vector = Vector::new(x, y, z);
-        vector.normalize();
-        vector
+        Vector::new(x, y, z).normalized()
     }
 
     pub fn norm(&self) -> f64 {
-        norm(self.x, self.y, self.z)
-    }
-
-    pub fn normalize(&mut self) {
-        let inverse_norm = 1.0 / self.norm();
-        self.x *= inverse_norm;
-        self.y *= inverse_norm;
-        self.z *= inverse_norm;
+        self.v.norm()
     }
 
     pub fn normalized(&self) -> Vector {
-        let mut return_vector = *self;
-        return_vector.normalize();
-        return_vector
+        Vector::new_from_na(self.v.normalize())
     }
 
     pub fn dot(&self, other_vector: Vector) -> f64 {
-        self.x * other_vector.x + self.y * other_vector.y + self.z * other_vector.z
+        self.dot(other_vector)
     }
 
     pub fn scale(&self, scalar: f64) -> Vector {
-        Vector::new(scalar * self.x, scalar * self.y, scalar * self.z)
+        Vector::new_from_na(self.v * scalar)
+    }
+
+    pub fn x(&self) -> f64 {
+        self.v[0]
+    }
+
+    pub fn y(&self) -> f64 {
+        self.v[1]
+    }
+
+    pub fn z(&self) -> f64 {
+        self.v[2]
     }
 }
 
@@ -86,7 +106,7 @@ impl Sub for Point {
     type Output = Vector;
 
     fn sub(self, other: Point) -> Self::Output {
-        Vector::new(self.x - other.x, self.y - other.y, self.z - other.z)
+        Vector::new_from_na(self.p - other.p)
     }
 }
 
@@ -94,7 +114,7 @@ impl Add<Vector> for Point {
     type Output = Point;
 
     fn add(self, other: Vector) -> Self::Output {
-        Point::new(self.x + other.x, self.y + other.y, self.z + other.z)
+        Point::new_from_na(self.p + other.v)
     }
 }
 
@@ -102,7 +122,7 @@ impl Add for Vector {
     type Output = Vector;
 
     fn add(self, other: Vector) -> Self::Output {
-        Vector::new(self.x + other.x, self.y + other.y, self.z + other.z)
+        Vector::new_from_na(self.v + other.v)
     }
 }
 
@@ -110,6 +130,6 @@ impl Sub for Vector {
     type Output = Vector;
 
     fn sub(self, other: Vector) -> Self::Output {
-        Vector::new(self.x - other.x, self.y - other.y, self.z - other.z)
+        Vector::new_from_na(self.v - other.v)
     }
 }
